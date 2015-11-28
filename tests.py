9e8adec25a45b1fc5c34362365abeb1622ff7d1e
gluker <glukerr@gmail.com>
2015-11-28 10:57:37 +0000

    tests

---
+++ tests.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from parse_comments import search_in_url as parse_comments
+from oracle import search_in_url as oracle
+
+test_cases = [
+    'http://rusisrael.livejournal.com/283608.html',
+    'http://rusisrael.livejournal.com/4366076.html',
+    'http://potrebitel-il.livejournal.com/22412050.html',
+    'http://dolboeb.livejournal.com/2858081.html',
+    'http://dolboeb.livejournal.com/2868126.html',
+    'http://tourism-il.livejournal.com/1003133.html',
+    'http://rabota-il.livejournal.com/9281433.html',
+    'http://rabota-il.livejournal.com/9069326.html',
+    'http://potrebitel-il.livejournal.com/22338502.html',
+    'http://rabota-il.livejournal.com/712789.html',
+    'http://ladies-il.livejournal.com/7098073.html',
+    ]
+
+for case in test_cases:
+    print("Trying {}".format(case))
+    try:
+        stable = oracle(case)
+    except Exception as ex:
+        print("Oracle felt on {} with {}".format(case,ex))
+        break
+    try:
+        testing = parse_comments(case)
+    except Exception as ex:
+        print("Test felt on {} with {}".format(case,ex))
+        break
+    assert parse_comments(case) == oracle(case), "Results don't match"
+
